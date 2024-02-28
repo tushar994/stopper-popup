1a23ca0ad778fe6c64451d96e81bcd30780d89e0
tushar994 <54453857+tushar994@users.noreply.github.com>
2024-02-29 04:52:35 +0530

    initial implementation

---
+++ scripts/content.js
@@ -0,0 +1,7 @@
+var scriptElement = document.createElement('script');
+
+// Set the source of the script
+scriptElement.src = alert("DO NOT CHILL HERE. FIND OUT SOMETHING ELSE. ANYTHING ELSE. PLEASE");
+
+// Append the script to the DOM
+(document.head || document.documentElement).appendChild(scriptElement);
